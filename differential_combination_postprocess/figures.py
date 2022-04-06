add stuff for yH and kind of fix bug with stupid overflow - remember to add it to the list when you add a new observable

differential_combination_postprocess/figures.py
@@ -6,7 +6,7 @@ hep.style.use("CMS")
 from itertools import cycle
 from copy import deepcopy
 
-from .cosmetics import rainbow, observable_specs, category_specs
+from .cosmetics import rainbow, observable_specs, category_specs, fit_type_colors
 from .shapes import ObservableShapeSM
 
 # Silence matplotlib warnings for Christ sake
@@ -65,9 +65,7 @@ class XSNLLsPerPOI:
             ax.axhline(1.0, color="k", linestyle="--")
 
             # Draw all the NLLs with different colors
-            rainbow_iter = cycle(rainbow)
-            for scan_tpl in scans:
-                color = next(rainbow_iter)
+            for scan_tpl, color in zip(scans, fit_type_colors):
                 ax = scan_tpl[1].plot(ax, color, label=scan_tpl[0])
                 ax = scan_tpl[1].plot_original_points(
                     ax, color, label=f"{scan_tpl[0]} (original)"
@@ -75,7 +73,7 @@ class XSNLLsPerPOI:
 
             # Legend
             ax.legend(loc="upper center", prop={"size": 10}, ncol=4)
-            hep.cms.label(loc=0, data=True, llabel="Work in Progress", lumi=35.9, ax=ax)
+            hep.cms.label(loc=0, data=True, llabel="Work in Progress", lumi=138, ax=ax)
 
             self.figures.append((fig, ax, output_name))
 
@@ -88,6 +86,39 @@ class XSNLLsPerPOI:
             fig.savefig(
                 "{}/{}.pdf".format(output_dir, output_name), bbox_inches="tight"
             )
+
+
+class XSNLLsPerPOI_Full(XSNLLsPerPOI):
+    def __init__(self, subcategory_spectra):
+        self.figures = []
+        nominal_spectrum = list(subcategory_spectra.values())[0]
+        pois = nominal_spectrum.scans.keys()
+
+        for poi in pois:
+            scans = []
+            for subcategory, spectrum in subcategory_spectra.items():
+                scans.append((subcategory, spectrum.scans[poi]))
+
+            fig, ax = plt.subplots()
+            output_name = f"Full_NLLs_{nominal_spectrum.variable}_{nominal_spectrum.category}_{poi}"
+
+            # Set labels
+            ax.set_xlabel(poi)
+            ax.set_ylabel("-$\Delta$lnL")
+
+            # Draw all the NLLs with different colors
+            rainbow_iter = cycle(rainbow)
+            for scan_tpl in scans:
+                color = next(rainbow_iter)
+                ax = scan_tpl[1].plot_original_points(
+                    ax, color, label=scan_tpl[0], for_single_plot=True
+                )
+
+            # Legend
+            ax.legend(loc="upper center", prop={"size": 10}, ncol=4)
+            hep.cms.label(loc=0, data=True, llabel="Work in Progress", lumi=138, ax=ax)
+
+            self.figures.append((fig, ax, output_name))
 
 
 class XSNLLsPerCategory(Figure):
@@ -119,9 +150,7 @@ class XSNLLsPerCategory(Figure):
 
         # Legend
         self.ax.legend(loc="upper center", prop={"size": 10}, ncol=4)
-        hep.cms.label(
-            loc=0, data=True, llabel="Work in Progress", lumi=35.9, ax=self.ax
-        )
+        hep.cms.label(loc=0, data=True, llabel="Work in Progress", lumi=138, ax=self.ax)
 
 
 class DiffXSsPerObservable(Figure):
@@ -179,7 +208,7 @@ class DiffXSsPerObservable(Figure):
         self.ratio_ax.grid(which="major", axis="x", linestyle="-", alpha=0.3)
 
         # Horrible way to somehow move the points away from each other and not have them superimposed
-        displacements = [0, 0.1, -0.1, 0.2, -0.2, 0.3, -0.3]
+        displacements = [0, 0.2, -0.2, 0.4, -0.4, 0.6, -0.6]
         categories = list(set([shape.category for shape in observable_shapes]))
         displacements_dict = {k: v for k, v in zip(categories, displacements)}
         logger.debug(f"Displacements: {displacements_dict}")
@@ -211,9 +240,12 @@ class DiffXSsPerObservable(Figure):
 
             displacement = displacements_dict[shape_systonly.category]
             shape_systonly.fake_maybe_moved_centers = (
-                shape.fake_centers + np.diff(shape.fake_edges) * displacement
+                shape_systonly.fake_centers
+                + np.diff(shape_systonly.fake_edges) * displacement
             )
 
+            prediction = deepcopy(sm_shape)
+            prediction.rebin(shape_systonly.edges)
             self.main_ax, self.ratio_ax = shape_systonly.plot_as_band(
                 self.main_ax, self.ratio_ax, prediction
             )
@@ -222,6 +254,6 @@ class DiffXSsPerObservable(Figure):
         self.main_ax.legend(loc="lower left")
 
         hep.cms.label(
-            loc=0, data=True, llabel="Work in Progress", lumi=35.9, ax=self.main_ax
+            loc=0, data=True, llabel="Work in Progress", lumi=138, ax=self.main_ax
         )
 

scripts/plot_xs_scans.py
@@ -11,6 +11,7 @@ from copy import deepcopy
 # Needed to fix the fucking
 # _tkinter.TclError: couldn't connect to display "localhost:12.0"
 import matplotlib
+import matplotlib.pyplot as plt
 
 matplotlib.use("AGG")
 
@@ -22,10 +23,11 @@ from differential_combination_postprocess.scan import Scan, DifferentialSpectrum
 from differential_combination_postprocess.figures import (
     XSNLLsPerCategory,
     XSNLLsPerPOI,
+    XSNLLsPerPOI_Full,
     DiffXSsPerObservable,
 )
 from differential_combination_postprocess.shapes import ObservableShapeFitted, sm_shapes
-from differential_combination_postprocess.physics import analyses_edges
+from differential_combination_postprocess.physics import analyses_edges, overflows
 
 import logging
 
@@ -95,6 +97,13 @@ def parse_arguments():
         help="Do not produce final plots",
     )
 
+    parser.add_argument(
+        "--no-nll",
+        action="store_true",
+        default=False,
+        help="Skip production of NLL plots",
+    )
+
     return parser.parse_args()
 
 
@@ -108,6 +117,10 @@ def get_shapes_from_differential_spectra(differential_spectra, observable):
         )
         # First: copy the finest possible shape (Hgg) and rebin it with what we need
         sm_rebinned_shape = deepcopy(sm_shapes[observable])
+        logging.debug(f"SM original xs: {sm_rebinned_shape.xs}")
+        logging.debug(
+            f"SM original xs_over_binwidth: {sm_rebinned_shape.xs_over_bin_width}"
+        )
         sm_rebinned_shape.rebin(analyses_edges[observable][simple_category])
         # Second: bin-wise multiply the new bin values by the values for mu computed in the scan
         mus = np.array([scan.minimum[0] for scan in spectrum.scans.values()])
@@ -122,6 +135,9 @@ def get_shapes_from_differential_spectra(differential_spectra, observable):
         )
         logging.debug(f"Ordered mus for category {category}: {mus}")
         logging.debug(f"SM rebinned xs: {sm_rebinned_shape.xs}")
+        logging.debug(
+            f"SM rebinned xs_over_binwidth: {sm_rebinned_shape.xs_over_bin_width}"
+        )
         weighted_bins = np.multiply(np.array(sm_rebinned_shape.xs), mus)
         logging.debug(f"Reweighted xs: {weighted_bins}")
         weighted_bins_up = np.multiply(np.array(sm_rebinned_shape.xs), mus_up)
@@ -136,8 +152,10 @@ def get_shapes_from_differential_spectra(differential_spectra, observable):
                 weighted_bins,
                 weighted_bins_down,
                 weighted_bins_up,
+                overflow=observable in overflows,
             )
         )
+        logging.debug(f"Just appended shape {shapes[-1]}")
 
     return shapes
 
@@ -178,9 +196,13 @@ def main(args):
         statonly_cat = f"{category}_statonly"
         asimov_statonly_cat = f"{category}_asimov_statonly"
 
+        sub_categories = [category, asimov_cat, statonly_cat, asimov_statonly_cat]
+        if args.no_nll:
+            sub_categories = [category, statonly_cat]
+
         # Plot scans for nominal, statonly, asimov and asimov_statonly
         sub_cat_spectra = {}
-        for sub_cat in [category, statonly_cat, asimov_cat, asimov_statonly_cat]:
+        for sub_cat in sub_categories:
             logger.info(f"Working on sub-category {sub_cat}")
             regex = re.compile(f"{sub_cat}(-[0-9])?$")
             categories_numbers = [
@@ -213,15 +235,22 @@ def main(args):
             if sub_cat == statonly_cat and sub_cat.split("_")[0] in systematic_bands:
                 differential_spectra_statonly[sub_cat] = diff_spectrum
 
-            plot_to_dump = XSNLLsPerCategory(diff_spectrum)
-            plot_to_dump.dump(output_dir)
+            if not args.no_nll:
+                plot_to_dump = XSNLLsPerCategory(diff_spectrum)
+                plot_to_dump.dump(output_dir)
+                plt.close("all")
 
         # Plot one figure per POI with nominal, statonly, asimov and asimov_statonly NLLs
-        logger.info(
-            "Now plotting one figure per POI with nominal, statonly, asimov and asimov_statonly NLLs (if found)"
-        )
-        poi_plots = XSNLLsPerPOI(sub_cat_spectra)
-        poi_plots.dump(output_dir)
+        if not args.no_nll:
+            logger.info(
+                "Now plotting one figure per POI with nominal, statonly, asimov and asimov_statonly NLLs (if found)"
+            )
+            poi_plots = XSNLLsPerPOI(sub_cat_spectra)
+            poi_plots.dump(output_dir)
+
+        if args.debug and not args.no_nll:
+            full_plot_to_dump = XSNLLsPerPOI_Full(sub_cat_spectra)
+            full_plot_to_dump.dump(output_dir)
 
     logger.debug(f"Differential spectra: {differential_spectra}")
 