efc2f09d4dfd480c7a6b88f63ea44736fe95040d
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-08-16 11:02:06 +0200

    update tk limits

--- differential_combination_postprocess/physics.py
+++ differential_combination_postprocess/physics.py
@@ -217,8 +217,8 @@ TK_models = {
     "yukawa_floatingBR": {"kappac": (-20, 20), "kappab": (-4, 6)},
     "top_coupdep_ctcg": {"ct": (0, 2), "cg": (-0.04, 0.08)},
     "top_floatingBR_ctcg": {"ct": (-3, 3), "cg": (-0.15, 0.15)},
-    "top_coupdep_ctcb": {},
-    "top_floatingBR_ctb": {},
+    "top_coupdep_ctcb": {"ct": (-3, 3), "cb": (-3, 3)},
+    "top_floatingBR_ctb": {"ct": (-3, 3), "cb": (-12, 12)},
 }
 
 
